c59c90746035c40cf3d7254f7147da05d515cc14
michaelong.2021@techpass.gov.sg <michaelong.2021@techpass.gov.>
2024-07-21 23:57:22 +0800

    chore: remove commented out code for /login route in handler.go

--- backend/pkg/http/rest/handler.go
+++ backend/pkg/http/rest/handler.go
@@ -39,7 +39,7 @@ func (h *Handler) RegisterRoutes() {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		}
 	})
-	// http.HandleFunc("/login", h.Login)
+
 	http.HandleFunc("/swear", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodGet:
