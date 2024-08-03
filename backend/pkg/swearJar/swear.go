a13184ad69c024d7042eddc248940a3bd38a42df
michaelong.2021@techpass.gov.sg <michaelong.2021@techpass.gov.>
2024-08-03 17:23:42 +0800

    feat(swearJar pkg): Update Swear and create SwearJar struts
    - Add `swearJarId` field to the Swear struct in the swearJar package

--- backend/pkg/swearJar/swear.go
+++ backend/pkg/swearJar/swear.go
@@ -1,13 +1,15 @@
 package swearJar
 
 import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Swear struct {
-	ObjectID primitive.ObjectID `bson:"_id,omitempty"`
-	UserID   primitive.ObjectID `bson:"user_id"`
-	DateTime time.Time          `bson:"swear_time"`
-	Active   bool               `bson:"active"`
+	ObjectID   primitive.ObjectID `bson:"_id,omitempty"`
+	UserID     primitive.ObjectID `bson:"user_id"`
+	DateTime   time.Time          `bson:"swear_time"`
+	Active     bool               `bson:"active"`
+	swearJarId primitive.ObjectID `bson:"swear_jar_id"`
 }
