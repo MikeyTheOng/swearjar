5bd99d527601f65ab73463101ba6ef10639a6c32
michaelong.2021@techpass.gov.sg <michaelong.2021@techpass.gov.>
2024-08-04 18:04:18 +0800

    feat: Separate authentication service and MongoDB repository layer by removing dependency on User type defined in service layer on MongoDB's primitive package

--- backend/pkg/authentication/user.go
+++ backend/pkg/authentication/user.go
@@ -1,12 +1,7 @@
 package authentication
 
-import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
-
 type User struct {
-	ObjectID primitive.ObjectID `bson:"_id,omitempty"`
-	Name     string             `bson:"name"`
-	Password string             `bson:"password"`
-	Email    string             `bson:"email"`
+	Name     string
+	Password string
+	Email    string
 }
