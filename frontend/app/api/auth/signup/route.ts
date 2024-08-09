1148b12b49eefbdb1c4197c1130ce64480c2561a
michaelong.2021@techpass.gov.sg <michaelong.2021@techpass.gov.>
2024-08-09 22:54:13 +0800

    feat: Configure auth.ts, Add login and signup API routes
    - Configure credentials provider in auth.ts
    - Add SessionProvider context in RootLayout
    - Added new API routes for login and signup functionality
    - Implemented validation using Zod schema for request body
    - Integrated with existing apiRequest utility method for backend queries
    - Handled error cases and returned appropriate JSON responses

---
+++ frontend/app/api/auth/signup/route.ts
@@ -0,0 +1,35 @@
+import { signUpSchema } from '@/lib/schema';
+import { apiRequest } from '@/lib/utils';
+import { ZodError } from 'zod';
+
+// /api/auth/signup
+export async function POST(request: Request) {
+    console.log("Sign up route")
+    try {
+        const body = await request.json();
+        const parsedBody = signUpSchema.parse(body);
+
+        const { response, status } = await apiRequest({
+            route: '/users?action=signup',
+            method: 'POST',
+            body: {
+                Name: parsedBody.Name,
+                Email: parsedBody.Email,
+                Password: parsedBody.Password,
+            },
+        });
+        return new Response(JSON.stringify(response), { status: status });
+    } catch (error) {
+        console.log("Error:", error)
+        let errorMessage = 'Unknown error';
+
+        if (error instanceof ZodError) {
+            errorMessage = error.errors.map((err) => `${err.path.join('.')} - ${err.message}`).join(', ');
+            console.error('Zod Validation Error:', error.errors);
+        } else if (error instanceof Error) {
+            errorMessage = error.message;
+        }
+        
+        return new Response(JSON.stringify({ error: errorMessage }), { status: 500 });
+    }
+}
