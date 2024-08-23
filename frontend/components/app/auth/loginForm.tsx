49a6fe373600cd91265849862ad53d573b079ecc
michaelong.2021@techpass.gov.sg <michaelong.2021@techpass.gov.>
2024-08-23 11:39:09 +0800

    feat: Login page gracefully handles any login errors

--- frontend/components/app/auth/loginForm.tsx
+++ frontend/components/app/auth/loginForm.tsx
@@ -7,6 +7,8 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import PasswordInput from "./passwordInput";
 import toast, { ErrorIcon } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface SignUpFormData {
     Email: string;
@@ -14,14 +16,17 @@ interface SignUpFormData {
 }
 
 export default function LoginForm() {
+    const [error, setError] = useState<boolean>(false);
+    const router = useRouter()
+
     const { register, handleSubmit, formState: { errors } } = useForm({
         defaultValues: {
-            Email: "",
-            Password: "",
+            // Email: "",
+            // Password: "",
 
             // ! Testing
-            // Email: "test1@gmail.com",
-            // Password: "12345678A!"
+            Email: "test1@gmail.com",
+            Password: "12345678A!"
         }
     });
 
@@ -29,29 +34,25 @@ export default function LoginForm() {
         try {
             const response = await signIn('credentials', {
                 callbackUrl: '/',
-                // redirect: false,
+                redirect: false,
                 Email: data.Email,
                 Password: data.Password,
             });
-            console.log("Response (login):", response);
 
-            if (!response || !response.ok) {
+            // console.log("Response (login):", response);
+            if (response?.error) {
+                setError(true);
                 const errorMessage = response?.error || response?.status || 'Unknown error';
-                toast(
-                    <span className='bg-background-100'>
-                        {errorMessage}
-                    </span>,
-                    {
-                        id: "login-error",
-                        duration: 1500,
-                        position: 'top-center',
-                        style: {
-                            background: 'var(--background)',
-                        },
-                        icon: <ErrorIcon />
-                    }
-                );
-                throw new Error(`Login failed: ${errorMessage}`);
+                console.error("Error message:", errorMessage);
+                // throw new Error(`Login failed: ${errorMessage}`);
+                toast.error("Check your email or password", {
+                    id: "login-error",
+                    duration: 1500,
+                    position: 'top-center',
+                    icon: <ErrorIcon />,
+                });
+            } else {
+                router.push('/');
             }
         } catch (error) {
             console.error('Login failed:', error);
@@ -61,6 +62,7 @@ export default function LoginForm() {
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <div className="grid w-full items-center gap-4">
+                {error && errorMsg()}
                 <div className="flex flex-col space-y-1.5">
                     <Label>Email</Label>
                     <Input id="email" placeholder="john.doe@example.com" {...register("Email", { required: "Email is required" })} />
@@ -79,5 +81,26 @@ export default function LoginForm() {
                 </div>
             </div>
         </form>
+    )
+}
+
+const errorMsg = () => {
+    return (
+        <div role="alert" className="daisy-alert daisy-alert-error bg-error/10 border-error text-error rounded-lg flex justify-center p-2">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 shrink-0 stroke-current"
+                fill="none"
+                viewBox="0 0 24 24">
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <span className="text-sm text-red-600">
+                Check your email or password
+            </span>
+        </div>
     )
 }
