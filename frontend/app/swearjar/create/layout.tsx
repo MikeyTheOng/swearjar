aa29d7fa971164b367f32cbb9262a65c0f622735
MikeyTheOng <cold.bubbling.tea@gmail.com>
2024-08-31 01:53:36 +0800

    feat: Move authentication check to CreateSJLayout

--- frontend/app/swearjar/create/layout.tsx
+++ frontend/app/swearjar/create/layout.tsx
@@ -1,13 +1,22 @@
-import Footer from "@/components/layout/footer";
+import { auth } from "@/auth";
+import { redirect } from 'next/navigation'
 
-export default function CreateSJLayout({
+import Footer from "@/components/layout/footer";
+import Navbar from "@/components/layout/navbar";
+
+export default async function CreateSJLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
+    const session = await auth()
+    if (!session) {
+        redirect('/auth/login')
+    }
     return (
         <section className="h-dvh flex flex-col">
-            <main className="flex-grow flex justify-center px-4">
+            <Navbar session={session} />
+            <main className="flex-grow flex justify-center px-4 mt-7">
                 {children}
             </main>
             <Footer />
--- frontend/app/swearjar/create/page.tsx
+++ frontend/app/swearjar/create/page.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from 'next'
-import { redirect } from 'next/navigation'
 
 import MainContent from "./mainContent";
-
 import BreadcrumbHeader from "@/components/layout/header/breadcrumbHeader"
-import { auth } from '@/auth';
 
 
 export const metadata: Metadata = {
@@ -13,17 +10,12 @@ export const metadata: Metadata = {
 }
 
 export default async function CreateSwearJarPage() {
-  const session = await auth();
-  if (session) {
-    return (
-      <section className="w-full md:w-[768px] lg:w-[864px]">
-        <div className='mb-4 w-full'>
-          <BreadcrumbHeader title="Create SJ" subtitle='Kick your bad habits today!' />
-        </div>
-        <MainContent />
-      </section>
-    );
-  } else {
-    redirect('/auth/login')
-  }
+  return (
+    <section className="w-full md:w-[768px] lg:w-[864px]">
+      <div className='mb-4 w-full'>
+        <BreadcrumbHeader title="Create SJ" subtitle='Kick your bad habits today!' />
+      </div>
+      <MainContent />
+    </section>
+  );
 }
