a1b44528a1e6b912be48652b7e482f22c62087c0
MikeyTheOng <cold.bubbling.tea@gmail.com>
2024-08-30 19:10:50 +0800

    style: Update mobile and desktop styles of sign up and login pages

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -4,7 +4,7 @@ import LoginForm from "@/components/app/auth/loginForm";
 export default function Login() {
   const greetingMessage = "Welcome Back to"
   return (
-    <main className="flex justify-center mt-[20vh]">
+    <main className="flex justify-center mt-[20vh] md:mt-0 md:items-center md:h-full">
       <AuthCard
         greetingMessage={greetingMessage}
         className="p-4 max-w-[380px] shadow-none border-transparent md:border-neutral-200 md:pt-8 md:pb-6 md:rounded-2xl md:bg-white"
--- frontend/app/auth/signup/page.tsx
+++ frontend/app/auth/signup/page.tsx
@@ -4,7 +4,7 @@ import SignUpForm from "@/components/app/auth/signUpForm";
 export default function SignUp() {
   const greetingMessage = "Welcome to"
   return (
-    <main className='flex justify-center mt-[15vh]'>
+    <main className='flex justify-center mt-[15vh] md:mt-0 md:items-center md:h-full'>
       <AuthCard greetingMessage={greetingMessage} className="p-4 max-w-[380px] shadow-none border-transparent md:border-neutral-200 md:pt-8 md:pb-6 md:rounded-2xl md:bg-white">
         <SignUpForm />
       </AuthCard>
