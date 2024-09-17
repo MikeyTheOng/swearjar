b172852af200c4512c197e4a33678d7669f1f4ca
MikeyTheOng <cold.bubbling.tea@gmail.com>
2024-09-17 10:54:30 +0800

    fix: Uncomment and ensure that localStorage is only accessed on client-side

--- frontend/components/layout/FloatingActionButton/index.tsx
+++ frontend/components/layout/FloatingActionButton/index.tsx
@@ -7,7 +7,7 @@ import { usePathname } from 'next/navigation';
 
 import { useMediaQuery } from 'usehooks-ts'
 import { useQuery } from '@tanstack/react-query';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { Button } from "@/components/ui/shadcn/button";
 import {
@@ -29,6 +29,7 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/shadcn/drawer";
+import { ChevronDown } from "lucide-react"
 import { PiTipJarLight } from "react-icons/pi";
 import SwearJarSelector from './SwearJarSelector';
 import ErrorAlert from '@/components/shared/ErrorAlert';
@@ -48,19 +49,20 @@ export default function FloatingActionButton({ userId }: { userId: string }) {
   });
   const isDesktop = useMediaQuery("(min-width: 768px)")
   const [open, setOpen] = useState(false)
-  const [selectedSwearJar, setSelectedSwearJar] = useState<SwearJarProp | null>(() => {
-    // const lastSelectedSJString = localStorage.getItem('lastSelectedSwearJar');
-    // const lastSelectedSJ: SwearJarProp | null = lastSelectedSJString ? JSON.parse(lastSelectedSJString) : null;
 
-    // if (lastSelectedSJ?.Owners) {
-    //   const isOwner = lastSelectedSJ.Owners.includes(userId);
-    //   if (isOwner) {
-    //     console.log("isOwner")
-    //     return lastSelectedSJ;
-    //   }
-    // }
-    return null;
-  });
+  const [selectedSwearJar, setSelectedSwearJar] = useState<SwearJarProp | null>(null);
+  useEffect(() => {
+    // Ensure localStorage is only accessed on the client-side
+    const lastSelectedSJString = localStorage.getItem('lastSelectedSwearJar');
+    const lastSelectedSJ: SwearJarProp | null = lastSelectedSJString ? JSON.parse(lastSelectedSJString) : null;
+
+    if (lastSelectedSJ?.Owners) {
+      const isOwner = lastSelectedSJ.Owners.includes(userId);
+      if (isOwner) {
+        setSelectedSwearJar(lastSelectedSJ);
+      }
+    }
+  }, [userId]);
   const { handleAddSwear } = useAddSwear(selectedSwearJar?.SwearJarId || "")
 
   if (isDesktop) {
@@ -104,13 +106,15 @@ export default function FloatingActionButton({ userId }: { userId: string }) {
     )
   }
 
-  // TODO: Add close button to top-right and replace with down arrow (discord)
   return (
     <Drawer open={open} onOpenChange={setOpen}>
       <DrawerTrigger className="flex items-center justify-center bottom-14 right-8 w-14 h-14 fixed bg-primary hover:bg-primary/70 rounded-full shadow-lg z-50 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2">
         <PiTipJarLight size={40} />
       </DrawerTrigger>
       <DrawerContent className="bg-white">
+        <Button className="absolute left-4 top-4 h-5 w-5 p-0 rounded-full bg-neutral-200 active:bg-neutral-400 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground" onClick={() => setOpen(false)}>
+          <ChevronDown className="h-4 aspect-square stroke-[3] text-foreground" />
+        </Button>
         {
           error ? <ErrorAlert message={error.message} /> :
             <>
@@ -135,9 +139,6 @@ export default function FloatingActionButton({ userId }: { userId: string }) {
                 >
                   Submit
                 </Button>
-                {/* <DrawerClose>
-                  <Button variant="outline">Cancel</Button>
-                </DrawerClose> */}
               </DrawerFooter>
             </>
         }
