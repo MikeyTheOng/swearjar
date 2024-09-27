40a2fc68392dc320d1bfa8e76b525b86c9980319
MikeyTheOng <cold.bubbling.tea@gmail.com>
2024-09-27 19:23:53 +0800

    chore: Add documentation

--- frontend/lib/schema.ts
+++ frontend/lib/schema.ts
@@ -1,5 +1,6 @@
 import { array, object, string } from 'zod';
 
+// Sign Up Schema
 export const signUpSchema = object({
     Name: string({ required_error: "Name is required" })
         .min(1, "Name is required"),
@@ -13,27 +14,29 @@ export const signUpSchema = object({
         .regex(/^(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).+$/, "Password must contain at least one uppercase letter and one special character"),
 });
 
+// Login Schema
 export const loginSchema = object({
     Email: string({ required_error: "Email is required" })
         .min(1, "Email is required")
         .email("Invalid email"),
     Password: string({ required_error: "Password is required" })
-        .min(1, "Password is required")
+        .min(1, "Password is required"),
 });
 
+// User Schema
 export const userSchema = object({
     UserId: string().min(1, 'UserId is required'),
     Email: string().email('Invalid email format'),
     Name: string().min(1, 'Name is required'),
 });
 
+// Swear Jar Schemas
 export const swearJarBaseSchema = object({
     Name: string().min(1, 'Title is required'),
     Desc: string().optional(),
     Owners: array(string()).optional(), // Owners + additionalOwners
 });
 
-// Add this new schema
 export const swearJarWithOwnersSchema = swearJarBaseSchema.extend({
     Owners: array(userSchema),
 });
