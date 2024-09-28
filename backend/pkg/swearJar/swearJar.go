2120433813202f494ad1c8f0b0bb1161a6bd7be6
MikeyTheOng <cold.bubbling.tea@gmail.com>
2024-09-28 18:32:03 +0800

    feat(swearjar pkg): Create new SwearJarWithOwners type

--- backend/pkg/swearJar/swearJar.go
+++ backend/pkg/swearJar/swearJar.go
@@ -2,6 +2,8 @@ package swearJar
 
 import (
 	"time"
+
+	"github.com/mikeytheong/swearjar/backend/pkg/authentication"
 )
 
 type SwearJarBase struct {
@@ -9,5 +11,13 @@ type SwearJarBase struct {
 	Name       string    `bson:"Name"`
 	Desc       string    `bson:"Desc"`
 	Owners     []string  `bson:"Owners"`
+	CreatedAt  time.Time `bson:"CreatedAt"`
+}
+
+type SwearJarWithOwners struct {
+    SwearJarId string    `bson:"_id,omitempty"`
+    Name       string    `bson:"Name"`
+	Desc       string    `bson:"Desc,omitempty"`
+	Owners     []authentication.UserResponse    `bson:"Owners"`
 	CreatedAt  time.Time `bson:"CreatedAt"`
 }
