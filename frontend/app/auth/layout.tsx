3e6853599226b1537c76cbf6105bf398c877d760
MikeyTheOng <cold.bubbling.tea@gmail.com>
2024-10-22 00:19:25 +0800

    feat: On all auth pages, if user is already signed in, redirect to `/swearjar/list`

--- frontend/app/auth/layout.tsx
+++ frontend/app/auth/layout.tsx
@@ -1,16 +1,25 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
 import Footer from "@/components/layout/footer"
 
-export default function AuthLayout({
+export default async function AuthLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
-    return (
-        <section className="w-full h-dvh flex flex-col">
-            <main className="flex-grow">
-                {children}
-            </main>
-            <Footer />
-        </section>
-    )
+    const session = await auth();
+    if (session?.user) {
+        redirect("/swearjar/list");
+    } else {
+        return (
+            <section className="w-full h-dvh flex flex-col">
+                <main className="flex-grow">
+                    {children}
+                </main>
+                <Footer />
+            </section>
+        )
+
+    }
 }
