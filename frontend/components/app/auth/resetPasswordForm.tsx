df08e9c9896aeb80e7a2702012fda31352835539
MikeyTheOng <cold.bubbling.tea@gmail.com>
2024-10-24 22:16:37 +0800

    feat: Verify auth token and display ResetPasswordForm or InvalidTokenAlert

--- frontend/components/app/auth/resetPasswordForm.tsx
+++ frontend/components/app/auth/resetPasswordForm.tsx
@@ -1,19 +1,17 @@
 "use client"
-import toast from 'react-hot-toast';
-import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import { useForm, SubmitHandler } from "react-hook-form";
+import { useRouter } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import { useSession } from 'next-auth/react';
-import { useState } from 'react';
+import { useMutation } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
 
-import {
-    Alert,
-    AlertDescription,
-} from "@/components/ui/shadcn/alert"
-import { Button } from "@/components/ui/shadcn/button"
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/shadcn/alert";
+import { Button } from "@/components/ui/shadcn/button";
+import { Label } from "@/components/ui/shadcn/label";
 import ErrorIcon from '@/components/shared/icons/animated/errorIcon';
-import { Input } from "@/components/ui/shadcn/input"
-import { Label } from "@/components/ui/shadcn/label"
-import { X } from "lucide-react"
+import { X } from "lucide-react";
 import PasswordInput from './passwordInput';
 
 
@@ -28,6 +26,42 @@ export default function ResetPasswordForm() {
     if (status === 'authenticated') {
         router.push('/swearjar/list');
     }
+
+    const searchParams = useSearchParams();
+    const token = searchParams.get('token');
+    const [isTokenVerified, setIsTokenVerified] = useState(false);
+
+    const { mutate: verifyToken, isIdle, isPending } = useMutation<Response, Error, string>({
+        mutationFn: async (encodedToken: string) => {
+            const response = await fetch('/api/auth/token/verify', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    Token: encodedToken,
+                    Purpose: 'PasswordReset',
+                }),
+            });
+            if (!response.ok) {
+                throw new Error('Token verification failed');
+            }
+            return response;
+        },
+        onSuccess: () => {
+            setIsTokenVerified(true);
+        },
+        onError: (error) => {
+            console.error('Error verifying token:', error);
+        }
+    });
+
+    useEffect(() => {
+        if (token) {
+            const encodedToken = encodeURIComponent(token);
+            verifyToken(encodedToken);
+        }
+    }, [token]);
 
     const { register, handleSubmit, formState: { errors } } = useForm({
         defaultValues: {
@@ -70,26 +104,43 @@ export default function ResetPasswordForm() {
             console.error('Forgot password failed:', error);
         }
     };
-
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="grid w-full items-center gap-4">
-                {showSuccessMessage && (
-                    <Alert variant="default" className='border-none'>
-                        <div className='flex justify-between items-center'>
-                            <AlertDescription className='text-xs md:text-sm text-input/80'>
-                                Password successfully reset!
-                            </AlertDescription>
-                            <X className="h-5 w-5 stroke-input/80 cursor-pointer" onClick={() => setShowSuccessMessage(false)} />
-                        </div>
-                    </Alert>
-                )}
-                <div className="flex flex-col space-y-1.5">
-                    <Label>Password</Label>
-                    <PasswordInput register={register} errors={errors} />
-                </div>
-                <Button className="w-full sm:font-semibold shadow-lg bg-gradient-to-r from-primary to-secondary hover:text-foreground hover:scale-105">Reset Password</Button>
+    if (isIdle || isPending) {
+        return (
+            <div className='flex items-center justify-center text-secondary'>
+                Verifying token &nbsp;
+                <span className="daisy-loading daisy-loading-spinner daisy-loading-md" />
             </div>
-        </form>
-    );
+        )
+    } else if (!isTokenVerified) {
+        return (
+            <Alert variant="destructive">
+                <AlertTitle>PASSWORD RESET TOKEN IS INVALID</AlertTitle>
+                <AlertDescription>
+                    Please try again.
+                </AlertDescription>
+            </Alert>
+        )
+    } else {
+        return (
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="grid w-full items-center gap-4">
+                    {showSuccessMessage && (
+                        <Alert variant="default" className='border-none'>
+                            <div className='flex justify-between items-center'>
+                                <AlertDescription className='text-xs md:text-sm text-input/80'>
+                                    Password successfully reset!
+                                </AlertDescription>
+                                <X className="h-5 w-5 stroke-input/80 cursor-pointer" onClick={() => setShowSuccessMessage(false)} />
+                            </div>
+                        </Alert>
+                    )}
+                    <div className="flex flex-col space-y-1.5">
+                        <Label>Password</Label>
+                        <PasswordInput register={register} errors={errors} />
+                    </div>
+                    <Button className="w-full sm:font-semibold shadow-lg bg-gradient-to-r from-primary to-secondary hover:text-foreground hover:scale-105">Reset Password</Button>
+                </div>
+            </form>
+        );
+    }
 }
