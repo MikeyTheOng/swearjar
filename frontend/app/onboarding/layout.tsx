d18ed8ff818990b124be2bb4374f0e4af2a235c3
MikeyTheOng <cold.bubbling.tea@gmail.com>
2024-10-25 19:12:09 +0800

    feat: Create '/onboarding` page

---
+++ frontend/app/onboarding/layout.tsx
@@ -0,0 +1,19 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
+import Footer from "@/components/layout/footer";
+
+export default async function AuthLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <section className="w-full h-dvh flex flex-col">
+            <main className="flex-grow">
+                {children}
+            </main>
+            <Footer />
+        </section>
+    );
+}
