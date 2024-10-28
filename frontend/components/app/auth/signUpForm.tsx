2a5a3480110a245ba5e283ecb261ccbbc55286c1
MikeyTheOng <cold.bubbling.tea@gmail.com>
2024-10-28 23:04:02 +0800

    feat:  Show success message on sign up

--- frontend/components/app/auth/signUpForm.tsx
+++ frontend/components/app/auth/signUpForm.tsx
@@ -1,9 +1,10 @@
 "use client"
+import ErrorMessage from '@/components/shared/ErrorMessage';
 import toast from 'react-hot-toast';
 import { useRouter } from 'next/navigation';
 import { useForm, SubmitHandler } from "react-hook-form";
 import { useSession } from 'next-auth/react';
-import ErrorMessage from '@/components/shared/ErrorMessage';
+import { useState } from 'react';
 
 import { Button } from "@/components/ui/shadcn/button"
 import { Input } from "@/components/ui/shadcn/input"
@@ -11,6 +12,7 @@ import { Label } from "@/components/ui/shadcn/label"
 import PasswordInput from "./passwordInput";
 import ErrorIcon from '@/components/shared/icons/animated/errorIcon';
 import Link from 'next/link';
+import { Alert, AlertTitle, AlertDescription } from '@/components/ui/shadcn/alert';
 
 interface SignUpFormData {
     Name: string;
@@ -18,22 +20,18 @@ interface SignUpFormData {
     Password: string;
 }
 export default function SignUp() {
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
     const router = useRouter();
-    const { data: session, status } = useSession();
+    const { status } = useSession();
     if (status === 'authenticated') {
         router.push('/swearjar/list');
     }
 
-    const { register, handleSubmit, formState: { errors } } = useForm({
+    const { register, handleSubmit, reset, formState: { errors } } = useForm({
         defaultValues: {
             Name: "",
             Email: "",
             Password: ""
-
-            // ! Testing
-            // Name: "Test",
-            // Email: "test1@gmail.com",
-            // Password: "12345678A!"
         }
     });
     const onSubmit: SubmitHandler<SignUpFormData> = async (data) => {
@@ -64,8 +62,10 @@ export default function SignUp() {
                 );
                 throw new Error(`Sign-up failed: ${errorData.error || response.statusText}`);
             }
-            const callbackUrl = `/onboarding`;
-            router.push(`/auth/login?callbackUrl=${callbackUrl}`);
+            // const callbackUrl = `/onboarding`;
+            // router.push(`/auth/login?callbackUrl=${callbackUrl}`);
+            setShowSuccessMessage(true);
+            reset();
         } catch (error) {
             console.error('Sign-up failed:', error);
         }
@@ -75,6 +75,7 @@ export default function SignUp() {
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <div className="grid w-full items-center gap-4">
+                {showSuccessMessage && <SuccessAlert />}
                 <div className="flex flex-col space-y-1.5">
                     <Label>Name</Label>
                     <Input id="name" placeholder="John Doe" {...register("Name", { required: "Name is required" })} />
@@ -95,5 +96,18 @@ export default function SignUp() {
                 </p>
             </div>
         </form>
+    )
+}
+
+const SuccessAlert = () => {
+    return (
+        <Alert variant="default" className='border-none'>
+            <AlertTitle>Sign Up Successful!</AlertTitle>
+            <div className='flex justify-between items-center'>
+                <AlertDescription className='text-xs md:text-sm text-input/80'>
+                    Please check your email for verification.
+                </AlertDescription>
+            </div>
+        </Alert>
     )
 }
