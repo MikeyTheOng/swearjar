5d9cd876704437e2058e45b51d0a0c25a67bc638
MikeyTheOng <cold.bubbling.tea@gmail.com>
2024-10-30 12:09:55 +0800

    feat: Comment out breadcrumb

--- frontend/components/layout/header/breadcrumbHeader.tsx
+++ frontend/components/layout/header/breadcrumbHeader.tsx
@@ -6,9 +6,9 @@ interface BreadcrumbHeaderProps {
 export default function BreadcrumbHeader({ title, subtitle }: BreadcrumbHeaderProps) {
     return (
         <header>
-            <div className="mt-2">
+            {/* <div className="mt-2">
                 <p>Home / SwearJars</p>
-            </div>
+            </div> */}
             <div className="mt-4">
                 <h1 className="text-4xl md:text-4xl font-bold tracking-tight">{title}</h1>
                 {subtitle && <p className="text-foreground/50 tracking-tighter">{subtitle}</p>}
