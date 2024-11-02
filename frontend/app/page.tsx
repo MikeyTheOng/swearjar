289d1cba7934a34fc89d34d5ee52b27239df823d
MikeyTheOng <cold.bubbling.tea@gmail.com>
2024-11-02 21:36:10 +0800

    feat: Update domain to ongspace.com

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,19 +4,4 @@ import { redirect } from 'next/navigation'
 export default function Home() {
   redirect('/swearjar/list')
 
-  return (
-    <html lang="en">
-      <head>
-        <title>SwearJar</title>
-        <meta name="description" content="Jar Your Habits" />
-
-        {/* Open Graph Meta Tags */}
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://swearjar.michaelongning.com" />
-        <meta property="og:title" content="SwearJar" />
-        <meta property="og:description" content="Jar Your Habits" />
-        <meta property="og:image" content="https://swearjar.michaelongning.com/default-image.jpg" />
-      </head>
-    </html>
-  )
 }
